e651b3f9ff56a5e9578e5caebd1ad65a541a6672
meshnesh <tonnie.nesh@gmail.com>
2016-03-05 20:48:55 +0300

    linked the button to the page

--- app/src/main/java/com/example/tune/navigation/MainActivity.java
+++ app/src/main/java/com/example/tune/navigation/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import navigation.demo.greetings;
 import navigation.demo.navigation;
 
 import static com.example.tune.navigation.R.*;
@@ -93,7 +94,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            fn.beginTransaction().replace(R.id.frame, new greetings()).commit();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
